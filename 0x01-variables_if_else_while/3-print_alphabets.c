a99963ecb49f001e5a2ffe9ab1415d9c840a8238
viewpossibilities <viewpossibilities@gmail.com>
2022-12-10 03:18:41 -0800

    Tazsk 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry
+ * Return: always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+	putchar(c);
+for (c = 'A'; c <= 'Z'; ++c)
+	putchar(c);
+putchar('\n');
+return (0);
+}
