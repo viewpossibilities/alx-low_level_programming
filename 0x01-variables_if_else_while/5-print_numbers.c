ed065be0eb67954e5edf835e57d6a243cf4ba324
viewpossibilities <viewpossibilities@gmail.com>
2022-12-10 04:10:13 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry
+ * Return: always 0
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+	printf("%d\n", i);
+	i++;
+}
+return (0);
+}
