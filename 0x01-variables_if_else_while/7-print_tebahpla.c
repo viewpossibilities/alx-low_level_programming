4e823a57f6e95fcbbf17c5aa845a154aca54f030
viewpossibilities <viewpossibilities@gmail.com>
2022-12-10 09:08:39 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry
+ * Return: always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+	putchar(c);
+putchar('\n');
+return (0);
+}
+
