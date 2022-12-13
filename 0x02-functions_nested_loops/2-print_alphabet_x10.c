b8dd336138b825fb5248e09a7b4fc30e97ba2844
viewpossibilities <viewpossibilities@gmail.com>
2022-12-13 14:22:12 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+while (i <= 10)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	i++;
+	_putchar('\n');
+}
+}
