85da3edc6e8746fca4d2c1c19c045937bca5a1e2
viewpossibilities <viewpossibilities@gmail.com>
2022-12-13 14:38:59 -0800

    Task 2.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 int i = 0;
 while (i < 10)
 {
-	char c = 'a';
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	i++;
-	_putchar('\n');
+char c = 'a';
+while (c <= 'z')
+{
+	_putchar(c);
+	c++;
+}
+i++;
+_putchar('\n');
 }
 }
