a95422835b3de010c2fb1d3328c486f7064258c1
viewpossibilities <viewpossibilities@gmail.com>
2022-12-13 11:12:42 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the character to stdout
+ * @c : the character to print
+ * Return: Always 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
