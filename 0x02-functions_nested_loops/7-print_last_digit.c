3d21e4ccff32864c356de7cac2da7e405316a933
viewpossibilities <viewpossibilities@gmail.com>
2022-12-14 12:10:15 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - entry
+ * @n: the number
+ * Return: returnnnn
+ */
+
+int print_last_digit(int n)
+{
+int id = n % 10;
+if (id < 0)
+	id = id * -1;
+_putchar(id + '0');
+return (0);
+}
