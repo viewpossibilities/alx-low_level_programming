8fa312d9e9b27e924fe3c81ef62a72504b322985
viewpossibilities <viewpossibilities@gmail.com>
2022-12-16 06:20:46 -0800

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _isupper
+ * _isupper - entry point
+ * @c: character to be check
  * Return: 1 if upper and 0 if not
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
