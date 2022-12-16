db0f7447f4177298e5d46b25763cbb6591e60c12
viewpossibilities <viewpossibilities@gmail.com>
2022-12-16 14:22:03 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,18 @@
 
 void print_line(int n)
 {
-	int i;
-	if (n <= 0)
+int i;
+if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\n');
+		_putchar('_');
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+	_putchar('\n');
 
-	}
+}
 }
