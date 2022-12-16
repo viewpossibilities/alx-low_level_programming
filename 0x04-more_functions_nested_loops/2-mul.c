9c8429097d0e72619e3591b5f6b25a2827789954
viewpossibilities <viewpossibilities@gmail.com>
2022-12-16 08:51:04 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: number to be multiplied
+ * @b: numbver to be multiplied
+ * Return: return a value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
