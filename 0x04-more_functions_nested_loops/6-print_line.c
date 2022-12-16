ce87c2f7db9cfae831fa24edf480074826688548
viewpossibilities <viewpossibilities@gmail.com>
2022-12-16 14:15:13 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,19 +4,21 @@
  * @n: character
  * Return: always 0
  */
+
 void print_line(int n)
 {
-	if (n > 0)
-	{
-	while (n)
-	{
-		_putchar('_');
-		n--;
-	}
-	}
-	else
+	int i;
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+
+	}
 }
