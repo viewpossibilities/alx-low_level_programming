036412d612806efb855560dfe0cec11668a490a5
viewpossibilities <viewpossibilities@gmail.com>
2022-12-16 09:22:25 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  print_numbers - entry point
+ *  Return: return nothing
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+	_putchar(c);
+_putchar('\n');
+}
