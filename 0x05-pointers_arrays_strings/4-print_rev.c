ddb019f85bee9b4c793fa245602e1e34c57bee90
viewpossibilities <viewpossibilities@gmail.com>
2022-12-19 22:48:24 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - entry
+ * @s: character
+ * Return: always 0
+ */
+
+void print_rev(char *s)
+{
+int count = 0;
+while (count >= 0)
+{
+if (s[count] == '\0')
+{
+break;
+count++;
+}
+for (count = 0; count >= 0; count--)
+{
+	_putchar(s[count]);
+	_putchar('\n);
+}
+}
+}
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -5,7 +5,8 @@
  * @c : the character to print
  * Return: Always 1
  */
+
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
