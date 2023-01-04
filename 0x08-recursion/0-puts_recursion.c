94e8d97b41a9109bb8fe15695e85ddc366d3006e
viewpossibilities <viewpossibilities@gmail.com>
2023-01-04 10:54:45 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * File: 0-puts_recursion.c
+ * Auth: possible
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ */
+#include "main.h"
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+	_putchar('\n');
+}
